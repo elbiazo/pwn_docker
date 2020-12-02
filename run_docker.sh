4ddb936990fcc2362117937279f8681082d90cd8
levin <eric.l.biazo@gmail.com>
2020-12-01 19:24:50 -0800

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -1,4 +1,3 @@
 glibc() {
-    docker run -it --rm --privileged -v $PWD:/src -v /home/dmo/libc-database:/root/libc-database \
-            -v /home/dmo/work/ctf/utils:/home/dmo/ctf/utils -w="/src" libc:$1
+    docker run -it --rm --privileged -v $PWD:/src -w="/src" libc:$1
 }
